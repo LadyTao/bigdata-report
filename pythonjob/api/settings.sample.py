ec295bab741d05fa7a2d9aa7f2caf83fa94f88a6
huanganqing <huanganqing@wondershare.cn>
2019-01-24 16:20:36 +0800

    [Feature] Add ci_related settings to settings.sample.py.

--- pythonjob/api/settings.sample.py
+++ pythonjob/api/settings.sample.py
@@ -2,9 +2,15 @@ es_host='10.14.1.127'
 port=8900
 
 
-renew_mysql_host = ''
+renew_mysql_host = '10.14.1.10'
 renew_mysql_port = 3306
-renew_mysql_user = ''
+renew_mysql_user = 'root'
 renew_mysql_password = ''
-renew_mysql_db = ''
+renew_mysql_db = 'data_user'
  
+ci_mysql_host = '10.14.1.10'
+ci_mysql_port = 3306
+ci_mysql_user = 'root'
+ci_mysql_password = ''
+ci_mysql_db = 'data_sale'
+
