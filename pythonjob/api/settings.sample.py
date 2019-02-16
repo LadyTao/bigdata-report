d354bfeb177bb2e36a7d0a02bfe4981067b84b3f
huanganqing <huanganqing@wondershare.cn>
2019-02-16 19:33:00 +0800

    [Feature] add ci_channel_* to settings.sample.py.

--- pythonjob/api/settings.sample.py
+++ pythonjob/api/settings.sample.py
@@ -14,3 +14,10 @@ ci_mysql_user = 'root'
 ci_mysql_password = ''
 ci_mysql_db = 'data_sale'
 
+ci_notes_db = 'analysis_platform'
+ci_channel_host = '192.168.11.43'
+ci_channel_port = 3306
+ci_channel_user = 'vp_store_read'
+ci_channel_password = 'ppYDC##821348'
+ci_channel_db = 'store'
+
